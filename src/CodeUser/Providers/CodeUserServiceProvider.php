e777db5676fc64085ac4661745d422cf29ebf28c
Luiz <argentinaluiz@gmail.com>
2016-06-23 15:00:28 -0300

    Integrando ACL com Laravel

--- src/CodeUser/Providers/CodeUserServiceProvider.php
+++ src/CodeUser/Providers/CodeUserServiceProvider.php
@@ -48,5 +48,6 @@ class CodeUserServiceProvider extends ServiceProvider
             return new Router();
         });
         $this->app->register(EventServiceProvider::class);
+        $this->app->register(AuthServiceProvider::class);
     }
 }
